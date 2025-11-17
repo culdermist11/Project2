a32a6c1101fb511f8efce6f5bb183dc2aaababc2
Ivan <kovalenkoivol9@gmail.com>
2025-11-17 17:57:30 +0100

    Zadanie 2

---
+++ Zadanie2.py
@@ -0,0 +1,13 @@
+# Zadanie 2
+
+x = float(input("Podaj pierwszą liczbę (x): "))
+y = float(input("Podaj drugą liczbę (y): "))
+z = float(input("Podaj trzecią liczbę (z): "))
+
+if x > y:
+    x, y = y, x
+if y > z:
+    y, z = z, y
+if x > y:
+    x, y = y, x
+print("Liczby od najmniejszej do największej:", x, y, z)
